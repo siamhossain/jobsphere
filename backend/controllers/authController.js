7455efff216bd0fc274aa8e5bedc1c903c01f9aa
siam <shiamhossain1995@gmail.com>
2026-04-13 20:50:44 +0600

    Add user login API controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const register = async (req, res) => {
     try {
@@ -29,4 +30,40 @@ export const register = async (req, res) => {
     catch (err) {
         res.status(500).json({ msg: "Server error" });
     }
+};
+
+
+// Login API
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        role: user.role,
+        name: user.name
+      }
+    });
+
+  } catch (err) {
+    res.status(500).json({ msg: "Server error" });
+  }
 };

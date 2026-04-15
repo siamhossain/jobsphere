cd23bae06d285964ef4214a887f9b6b2cdb63cd4
siam <shiamhossain1995@gmail.com>
2026-04-15 13:07:44 +0600

    Add authMiddleware to verify token

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,27 @@
+// middleware/authMiddleware.js
+const jwt = require("jsonwebtoken");
+
+exports.protect = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ msg: "No token" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Invalid token" });
+  }
+};
+
+exports.adminOnly = async (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(403).json({ msg: "Admin only" });
+  }
+  next();
+};
--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const jobController = require("../controllers/jobController");
+const { protect, adminOnly } = require("../middleware/authMiddleware");
 
 router.get("/", jobController.getJobs);
 router.get("/featured", jobController.getFeaturedJobs);
 router.get("/latest", jobController.getLatestJobs);
 router.get("/:id", jobController.getSingleJob);
 
-router.post("/", jobController.createJob);
-router.delete("/:id", jobController.deleteJob);
-router.put("/:id", jobController.updateJob);
+router.post("/", protect, adminOnly, jobController.createJob);
+router.delete("/:id", protect, adminOnly, jobController.deleteJob);
+router.put("/:id", protect, adminOnly, jobController.updateJob);
 
 module.exports = router;
